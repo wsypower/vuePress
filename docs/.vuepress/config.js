043bf4524498bbadaa5b6ecd32b35bcd31f25490
weiyafei <2553241022@qq.com>
2019-03-04 19:52:22 +0800

    第一次服务器可以运行

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    title: '天翼智慧城市——智慧城管事业部',// 设置网站标题
+    description: '项目API与功能实现简述',
+    head: [
+        ['link', { rel: 'icon', href: '/img/logo.ico' }],
+        ['link', { rel: 'manifest', href: '/manifest.json' }],
+    ]，
+}
