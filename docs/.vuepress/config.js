aa0a7575892382eca5fe7a3b3760de96ba235700
weiyafei <2553241022@qq.com>
2019-03-05 16:35:31 +0800

    配置首页

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,8 +1,29 @@
 module.exports = {
-    title: '天翼智慧城市——智慧城管事业部',// 设置网站标题
-    description: '项目API与功能实现简述',
-    head: [
-        ['link', { rel: 'icon', href: '/img/logo.ico' }],
-        ['link', { rel: 'manifest', href: '/manifest.json' }],
-    ]
+	base: '/',
+	title: '天翼智慧城市',
+	description: `智慧城管事业部文档`,
+	head: [
+		['link', { rel: 'icon', href: `/favicon.ico` }]
+	],
+	repo: 'https://github.com/lewiscutey/blog',
+	ga: '',
+	serviceWorker: true,
+	evergreen: true,
+	themeConfig: {
+		background: `/img/`,
+		github: 'lewiscutey',
+		logo: '/img/logo.png',
+		accentColor: '#ac3e40',
+		per_page: 6,
+		date_format: 'yyyy-MM-dd HH:mm:ss',
+		nav: [
+			{text: 'Blog', link: '/blog/'},
+			{text: 'About', link: '/about/'},
+			{text: 'CSDN', link: 'http://www.cnblogs.com/lewiscutey/'},
+			{text: 'Github', link: 'https://github.com/lewiscutey'}
+		],
+
+         sidebarDepth : 2,
+         lastUpdated: 'Last Updated',
+	},
 }
